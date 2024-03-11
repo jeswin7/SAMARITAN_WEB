512da3eb6639472eb3e56d641fef443bba246963
jeswin7 <jeswinjohncy@gmail.com>
2024-03-11 18:34:31 -0400

    Added basic branding - brand name, logo

---
+++ samaritan/src/components/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <img :src="logo" class="logo" >
+    <div class="brand">{{brand }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    brand: String
+  },
+  data(){
+    return{
+      logo: require("../assets/samaritanLogo.png")
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello{
+  display: flex;
+  align-items: center;
+}
+.logo{
+  width: 100px
+}
+.brand{
+  color: #319F9F;
+  font-size: 40px;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
